deea78c81f0a186dc9f304ba7192f8d715872056
swan.benoit <swan.benoit@visiativ.com>
2023-11-08 20:14:47 +0100

    configure typescript and jest

---
+++ main.test.ts
@@ -0,0 +1,9 @@
+describe('test', function () {
+    it('should fail', function () {
+        expect(true).toBe(false);
+    });
+
+    it('should pass', function () {
+        expect(true).toBe(true);
+    });
+});
