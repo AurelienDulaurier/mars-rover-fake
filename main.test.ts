59410fbdb29f8e1c953b2b09a0e46d038fd6a414
swan <swan.benoit@visiativ.com>
2023-11-27 20:38:40 +0100

    refactor: move code to src directory

--- main.test.ts
+++
@@ -1,9 +0,0 @@
-describe('test', function () {
-    it('should fail', function () {
-        expect(true).toBe(false);
-    });
-
-    it('should pass', function () {
-        expect(true).toBe(true);
-    });
-});
